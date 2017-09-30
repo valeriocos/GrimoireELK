55481dc929cd248f30909d894ab1f416b3b19f42
Alvaro del Castillo <acs@bitergia.com>
2017-09-30 08:07:25 +0200

    [mongo2es] Allow to import all metrics from OSSMiner in a mongodb

--- utils/mongo2es.py
+++ utils/mongo2es.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 # Copy JSON items from MongoDB to Elasticsearch
+# If the collection is a OSSMeter one add project and other fields to items
 #
-# Copyright (C) 2016 Bitergia
+# Copyright (C) 2017 Bitergia
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,20 +25,19 @@
 #
 
 import argparse
-import json
 import logging
-import os
+
+from pprint import pprint
 
 from pymongo import MongoClient
-
-from dateutil import parser
 
 from grimoire_elk.elk.elastic import ElasticSearch
 
 def get_params():
     parser = argparse.ArgumentParser(usage="usage: mongo2es [options]",
                                      description="Import mongo items in ElasticSearch")
-    parser.add_argument("-c", "--collection", required=True, help="MongoDB Collection")
+    parser.add_argument("-a", "--all-collections", action='store_true', help="Process all MongoDB Collections")
+    parser.add_argument("-c", "--collection", help="MongoDB Collection")
     parser.add_argument("-m", "--mongo-host", default='localhost', help="MongoDB Host")
     parser.add_argument("-p", "--mongo-port", default='27017', type=int, help="MongoDB Port")
     parser.add_argument("-e", "--elastic-url", required=True, help="ElasticSearch URL")
@@ -45,9 +45,13 @@ def get_params():
     parser.add_argument('-g', '--debug', dest='debug', action='store_true')
     args = parser.parse_args()
 
+    if not args.collection and not args.all_collections:
+        parser.error("--collection or --all-collections needed")
+
     return args
 
-def fetch_mongodb(collection_str, host=None, port=None):
+def connect_to_mongo(host=None, port=None):
+    """ Return a connection to the mongo server in host and port """
     if host and port:
         client = MongoClient(host, port)
     elif host:
@@ -55,9 +59,90 @@ def fetch_mongodb(collection_str, host=None, port=None):
     else:
         client = MongoClient()
 
+    return client
+
+def is_ossmeter_historic_collection(collection):
+    # Check if a collection is an OSSMeter one
+    # Sample: modeling-graphiti.historic.newsgroups.articles
+
+    is_ossmeter = False
+
+    if len(collection.split(".")) == 4:
+        is_ossmeter = True
+
+    return is_ossmeter
+
+def fetch_mongodb_all(host=None, port=None):
+    logging.info("Searching for all OSSMeter metrics collections")
+    client = connect_to_mongo(host, port)
+
+    # Find all OSSMeter collections in mongo
+    for db in client.database_names():
+        for collection in client[db].collection_names():
+            collection_name = db + '.' + collection
+            if is_ossmeter_historic_collection(collection_name):
+                logging.debug('Loading items from %s', collection_name)
+                for item in fetch_mongodb_collection(collection_name, client=client):
+                    yield item
+
+def enrich_ossminer_item(item):
+    # Given a ossminer item enrich it to be used in Kibana
+
+    # Find the metric value field
+    common_fields = ['__date', '_type', '_id', '__datetime', 'bugData', 'newsgroups']
+    value_fields = list(set(item.keys()) - set(common_fields))
+    if len(value_fields) == 1:
+        item['metric_value'] = item[value_fields[0]]
+    elif len(value_fields) == 2:
+        # We also the cumulative metric
+        for field in value_fields:
+            value = item[field]
+            if not isinstance(item[field], (int, float)):
+                value = None
+            if 'cumulative' in field:
+                item['metric_es_value_cumulative'] = value
+            else:
+                item['metric_es_value'] = value
+    else:
+        logging.error("Can't find the metric value field %s", value_fields)
+
+    if '__datetime' in item:
+        item['datetime'] = item['__datetime'].isoformat()
+        item['__datetime'] = item['__datetime'].isoformat()
+    if '__date' in item:
+        item['date'] = item['__date']
+    # Field [_id] is a metadata field and cannot be added inside a document.
+    item['mongo_id'] = item.pop('_id')  # The _id can not in the data in ES
+    item['mongo_type'] = item.pop('_type')  # The _id can not in the data in ES
+
+    return item
+
+
+def fetch_mongodb_collection(collection_str, host=None, port=None, client=None):
+    """ conn could be a already created connection to Mongo """
+    if not client:
+        client = connect_to_mongo(host, port)
+
     collection = None
+    item_meta = {
+        'project': None,
+        'metric_type': None,
+        'metric_class': None,
+        'metric_name': None
+    }
     if "." in collection_str:
+        # Sample: modeling-graphiti.historic.newsgroups.articles
         subcollections = collection_str.split(".")
+        if len(subcollections) != 4:
+            logging.warning('%s is not a OSSMeter collection', collection_str)
+        else:
+            item_meta = {
+                'project': subcollections[0],
+                'metric_type': subcollections[1],
+                'metric_class': subcollections[2],
+                'metric_name': subcollections[3]
+            }
+
         for col in subcollections:
             client = client[col]
         collection = client
@@ -65,12 +150,8 @@ def fetch_mongodb(collection_str, host=None, port=None):
         collection = client[collection_str]
 
     for item in collection.find():
-        item['datetime'] = item['__datetime'].isoformat()
-        item['__datetime'] = item['__datetime'].isoformat()
-        item['date'] = item['__date']
-        # Field [_id] is a metadata field and cannot be added inside a document.
-        item['mongo_id'] = item.pop('_id')  # The _id can not in the data in ES
-        item['mongo_type'] = item.pop('_type')  # The _id can not in the data in ES
+        item = enrich_ossminer_item(item)
+        item.update(item_meta)
         yield item
 
 if __name__ == '__main__':
@@ -87,5 +168,13 @@ if __name__ == '__main__':
 
     elastic = ElasticSearch(args.elastic_url, args.index)
 
-    mongo_items = fetch_mongodb(args.collection, args.mongo_host, args.mongo_port)
-    elastic.bulk_upload_sync(mongo_items, "mongo_id")
+    if args.collection:
+        mongo_items = fetch_mongodb_collection(args.collection, args.mongo_host, args.mongo_port)
+    elif args.all_collections:
+        mongo_items = fetch_mongodb_all(args.mongo_host, args.mongo_port)
+    else:
+        raise RuntimeError('Collection to be processed not provided')
+
+    if mongo_items:
+        logging.info("Loading collections in Elasticsearch")
+        elastic.bulk_upload_sync(mongo_items, "mongo_id")
